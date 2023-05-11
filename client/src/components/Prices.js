c0f7c169b696e79b3a649a33bea906661023173c
prabhjyot9 <prabhjyot.wadhwa@gmail.com>
2023-05-11 22:32:07 +0530

    SearchBar Added

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "Less Than Rs. 2000",
+      array: [0, 1999],
+    },
+    {
+      _id: 1,
+      name: "rs. 2000 to Rs. 10000",
+      array: [2000, 9999],
+    },
+    {
+      _id: 2,
+      name: "Rs. 10000 to Rs. 20000",
+      array: [10000, 19999],
+    },
+    {
+      _id: 3,
+      name: "Rs. 20000 to Rs. 40000",
+      array: [20000, 39999],
+    },
+    {
+      _id: 4,
+      name: "Rs. 40000 to Rs. 80000",
+      array: [40000, 79999],
+    },
+    {
+      _id: 4,
+      name: "More Than Rs. 80000",
+      array: [80000, 200000],
+    },
+  ];
