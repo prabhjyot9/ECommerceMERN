cd5c2d34f268173212babd77b88055c95fc29293
prabhjyot9 <prabhjyot.wadhwa@gmail.com>
2023-05-11 07:07:33 +0530

    category added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoute.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
 import cors from "cors";
 
 //configure env
@@ -22,14 +23,15 @@ app.use(morgan("dev"));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/category", categoryRoutes);
 
 //rest api
 app.get("/", (req, res) => {
-  res.send("<h1> Welcome to ecommerce app </h1>");
+  res.send("<h1>Welcome to ecommerce app</h1>");
 });
 
 //PORT
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
